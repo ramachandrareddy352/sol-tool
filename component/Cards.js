3d448f9758e26fce8cc033d7e618779160707024
tavanam rama <rcrtavanam@gmail.com>
2026-01-13 20:28:07 +0530

    development images are added

--- component/Cards.js
+++ component/Cards.js
@@ -12,7 +12,7 @@ const Cards = () => {
       title_ko: "솔라나 ICO 웹사이트",
       description_ko:
         "우리는 Solana 기반 프로젝트를 위한 효과적인 자금 조달을 지원하는 ICO 플랫폼을 제공합니다.",
-      image: "/dummy-card-image.png",
+      image: "/1.ico.jpg",
       link: "https://t.me/mcret",
     },
     {
@@ -22,7 +22,7 @@ const Cards = () => {
       title_ko: "솔라나 MLM 웹사이트",
       description_ko:
         "우리는 Solana 네트워크에서 안정적이고 투명한 구조를 가진 MLM 웹사이트를 개발합니다.",
-      image: "/dummy-card-image.png",
+      image: "/2.Solana MLM Website.jpg",
       link: "https://t.me/mcret",
     },
     {
@@ -32,7 +32,7 @@ const Cards = () => {
       title_ko: "솔라나 토큰 에어드랍 웹사이트",
       description_ko:
         "우리는 Solana 토큰을 배포하기 위한 사용자 친화적인 인터페이스를 갖춘 플랫폼을 제공합니다.",
-      image: "/dummy-card-image.png",
+      image: "/3,Solana Token Airdrop Website.jpg",
       link: "https://t.me/mcret",
     },
     {
@@ -42,7 +42,7 @@ const Cards = () => {
       title_ko: "솔라나 스테이킹 웹사이트",
       description_ko:
         "우리는 Solana 네트워크에서 보상을 극대화할 수 있는 스테이킹 솔루션을 설계합니다.",
-      image: "/dummy-card-image.png",
+      image: "/4.Solana Staking Website.jpg",
       link: "https://t.me/mcret",
     },
     {
@@ -52,7 +52,7 @@ const Cards = () => {
       title_ko: "맞춤형 DApps",
       description_ko:
         "우리는 Solana 블록체인 기술을 사용하여 비즈니스 요구에 최적화된 맞춤형 DApps를 개발합니다.",
-      image: "/dummy-card-image.png",
+      image: "/5.Solana Staking Website.jpg",
       link: "https://t.me/mcret",
     },
     {
@@ -62,7 +62,7 @@ const Cards = () => {
       title_ko: "솔라나 토큰 거래 생성 서비스",
       description_ko:
         "우리는 Solana 블록체인에서 토큰 거래를 자동화하는 솔루션을 제공합니다.",
-      image: "/dummy-card-image.png",
+      image: "/6.Solana Token Transaction.jpg",
       link: "https://t.me/mcret",
     },
     {
@@ -72,7 +72,7 @@ const Cards = () => {
       title_ko: "솔라나 토큰 보유자 성장 서비스",
       description_ko:
         "우리는 자동으로 토큰 보유자를 증가시키는 봇 서비스를 제공합니다.",
-      image: "/dummy-card-image.png",
+      image: "/7.Solana Token Transaction.jpg",
       link: "https://t.me/mcret",
     },
     {
@@ -82,7 +82,7 @@ const Cards = () => {
       title_ko: "거래소 MM 마켓 메이킹 봇",
       description_ko:
         "우리는 중앙 집중식 거래소에서 초기 시장 환경을 조성하기 위한 마켓 메이킹 봇을 개발합니다.",
-      image: "/dummy-card-image.png",
+      image: "/8.Exchange MM Market Making Bot.jpg",
       link: "https://t.me/mcret",
     },
     {
@@ -92,7 +92,7 @@ const Cards = () => {
       title_ko: "거래소 상장",
       description_ko:
         "우리는 BingX, MEXC, LBank, LTX, XT, P2B, Coinstore, Indodax, Coin Market Cap 등의 거래소 상장을 지원합니다.",
-      image: "/dummy-card-image.png",
+      image: "/9.Exchange Listing.jpg",
       link: "https://t.me/mcret",
     },
   ];
